0c443de84b0618803ad3bfdc0a1c6d915c9b3110
jordanvogel <jordan.n.vogel@gmail.com>
2016-10-03 14:36:56 -0600

    LICENSE

--- app/src/main/java/jvogel/as1/Listener.java
+++ app/src/main/java/jvogel/as1/Listener.java
@@ -1,3 +1,31 @@
+/*
+ * (C) Copyright 2016 Jordan Vogel.
+ * All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Contributors: Abram Hindle - I used the Student Picker Saga as a guide
+ * "MadProgrammer" - http://stackoverflow.com/questions/12575990/calendar-date-to-yyyy-mm-dd-format-in-java
+ * "Raghunandan" - http://stackoverflow.com/questions/17453297/passing-arraylist-of-string-arrays-from-one-activity-to-another-in-android
+ * "user2216292" - http://stackoverflow.com/questions/17453297/passing-arraylist-of-string-arrays-from-one-activity-to-another-in-android
+ *
+ */
+
 package jvogel.as1;
 
 /**
